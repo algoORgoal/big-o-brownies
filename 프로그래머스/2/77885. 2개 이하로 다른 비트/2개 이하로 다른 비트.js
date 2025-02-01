6e012acfa61381b2cbc1a340ce28b3c1b78a079a
Chan <qufstar46@gmail.com>
2025-02-01 19:22:18 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 366.78 ms, Memory: 89.9 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.js
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.js
@@ -1,4 +1,17 @@
 function solution(numbers) {
-    var answer = [];
-    return answer;
+    const binaryStirngNumbers = numbers.map((number) => number.toString(2).padStart(50, '0'));
+    
+    const largerBinaryStringNumbers = binaryStirngNumbers.map((binaryString) => {
+        const lsb0Index = binaryString.length - 1 - [ ...binaryString ].reverse().findIndex((character) => character === '0');
+        if (lsb0Index === binaryString.length - 1) {
+            return binaryString.slice(0, lsb0Index).concat('1');
+        }
+        
+        return binaryString.slice(0, lsb0Index).concat('1').concat('0').concat(binaryString.slice(lsb0Index + 2));
+    });
+    
+    const largerNumbers = largerBinaryStringNumbers.map((binaryStringNumber) => parseInt(binaryStringNumber, 2));
+    
+    return largerNumbers;
+    
 }
