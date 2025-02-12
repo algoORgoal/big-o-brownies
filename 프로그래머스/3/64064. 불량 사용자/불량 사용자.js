095cbd6a7707acc8898d4d8e84eb2da0ee75fc52
Chan <qufstar46@gmail.com>
2025-02-12 11:53:52 +0900

    [level 3] Title: 불량 사용자, Time: 1.11 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.js
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.js
@@ -2,6 +2,10 @@
 // Run dfs with bitmask to see if each node has been visited or not
 // When all bits are masked, increase count
 
+// Create a lookup table whose keys are bannedId and values are userIds
+// Run dfs with bitmask to see if each node has been visited or not
+// When all bits are masked, increase count
+
 function generateIdTable(userIds, bannedIds) {
     return bannedIds.reduce((accumulator, bannedId, bannedIdIndex) => {
         const word = bannedId.split(/([*]+)/).map((token) => token[0] === '*' ? `.{${token.length}}` : token).join("");
@@ -41,6 +45,41 @@ function solution(userIds, bannedIds) {
                 visited: visited | (1 << userIdIndex),
                 });
                 visitedSet.add(visited | (1 << userIdIndex));
+            }
+        });
+    }
+    
+    return count;
+}
+
+function solution(userIds, bannedIds) {
+    const idTable = generateIdTable(userIds, bannedIds);
+    
+    const root = bannedIds[0];
+    const stack = [{ 
+        nodeIndex: 0, 
+        combination: 0,
+    }];
+    
+    let count = 0;
+    const visitedSet = new Set();
+    
+    while (stack.length) {
+        const { nodeIndex, combination } = stack.pop();
+        
+        if (nodeIndex === bannedIds.length) {
+            count += 1;
+            continue;
+        }
+        
+        const userIdIndices = idTable[nodeIndex]
+        userIdIndices.forEach((userIdIndex) => {
+            if (!visitedSet.has(combination | (1 << userIdIndex))) {
+                stack.push({
+                nodeIndex: nodeIndex + 1,
+                combination: combination | (1 << userIdIndex),
+                });
+                visitedSet.add(combination | (1 << userIdIndex));
             }
         });
     }
