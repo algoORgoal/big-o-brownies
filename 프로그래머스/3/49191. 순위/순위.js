ab1e31bb7bcd9712dea08f501dc1590c1bba5162
Chan <qufstar46@gmail.com>
2025-02-17 15:38:18 +0900

    [level 3] Title: 순위, Time: 33.16 ms, Memory: 38.8 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.js
+++ 프로그래머스/3/49191. 순위/순위.js
@@ -1,3 +1,14 @@
+// Represent the relative magnitude as a directed graph.
+// if A beats B, there is an edge from A to B.
+// if A beats B and B beats C, there is a path from A to C.
+// if A is ranked higher than B and B is ranked higher than C, there is a path from A to C.
+// if there is not path from A to C, the relationship is not clear.
+// Run floyd-warshall algorithm to check if there is a path from node i to j.
+// if i has path from i to k or k to i, for 1<=k<=n(k!=i), we can clear set the order of the node. Otherwise, it's impossible.
+// So check dp[i][k] || dp[k][i] for every k except j. If so, add count to 1.
+// Time complexity: O(n^3) 
+// Space complexity: O(n^2) 
+
 const json = (value) => JSON.stringify(value);
 
 function solution(n, results) {
