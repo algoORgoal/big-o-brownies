780188290dd6244ba12f17d17c6a481f6b536b82
Chan <qufstar46@gmail.com>
2025-03-10 01:33:40 +0900

    [level 3] Title: 기둥과 보 설치, Time: 12.99 ms, Memory: 42 MB -BaekjoonHub

--- 프로그래머스/3/60061. 기둥과 보 설치/README.md
+++ 프로그래머스/3/60061. 기둥과 보 설치/README.md
@@ -1,10 +1,10 @@
 # [level 3] 기둥과 보 설치 - 60061 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60061) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60061#) 
 
 ### 성능 요약
 
-메모리: 41.6 MB, 시간: 17.68 ms
+메모리: 42 MB, 시간: 12.99 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 10일 00:39:42
+2025년 03월 10일 01:33:38
 
 ### 문제 설명
 
--- 프로그래머스/3/60061. 기둥과 보 설치/기둥과 보 설치.js
+++ 프로그래머스/3/60061. 기둥과 보 설치/기둥과 보 설치.js
@@ -1,4 +1,54 @@
-function solution(n, build_frame) {
-    var answer = [[]];
-    return answer;
+const isValid = (matrix, x, y) => x >= 0 && x < matrix.length && y >= 0 && y < matrix[0].length;
+
+function isCommandValid(matrices, position) {
+    const [ verticalMatrix, horizontalMatrix ] = matrices;
+    const [ x, y ] = position;
+    for (let i = x - 1; i <= x + 1; i++) {
+        for (let j = y - 1; j <= y + 1; j++) {
+            if (isValid(verticalMatrix, i, j) && verticalMatrix[i][j]) {
+                const isVerticalValid = 
+                i === 0 
+                || (isValid(horizontalMatrix, i, j - 1) && horizontalMatrix[i][j - 1])
+                || (isValid(horizontalMatrix, i, j) && horizontalMatrix[i][j])
+                || (isValid(verticalMatrix, i - 1, j) && verticalMatrix[i - 1][j]);
+                if (!isVerticalValid) return false;
+            }
+            if (isValid(horizontalMatrix, i, j) && horizontalMatrix[i][j]) {
+                const isHorizontalValid = 
+                isValid(verticalMatrix, i - 1, j) && verticalMatrix[i - 1][j]
+                || isValid(verticalMatrix, i - 1, j + 1) && verticalMatrix[i - 1][j + 1]
+                || (isValid(horizontalMatrix, i, j - 1) && horizontalMatrix[i][j - 1]
+                && isValid(horizontalMatrix, i, j + 1) && horizontalMatrix[i][j + 1]);
+                if (!isHorizontalValid) return false;
+            }
+        }
+    }
+    
+    return true;
+}
+
+
+function solution(n, commands) {
+    const verticalMatrix = Array.from({ length: n + 1 }, () => Array.from({ length: n + 1 }, () =>0));
+    const horizontalMatrix = Array.from({ length: n + 1 }, () => Array.from({ length: n + 1, }, () => 0));
+    commands.forEach(([ y, x, direction, type ]) => {
+        if (direction === 0) {
+            verticalMatrix[x][y] = type;
+            if (!isCommandValid([ verticalMatrix, horizontalMatrix ], [ x, y ])) verticalMatrix[x][y] ^= 1;
+        } else {
+            horizontalMatrix[x][y] = type;
+            if (!isCommandValid([ verticalMatrix, horizontalMatrix ], [ x, y ])) horizontalMatrix[x][y] ^= 1;
+        }
+    });
+        
+    
+    const frames = [];
+    for (let i = 0; i < verticalMatrix.length; i++) { 
+        for (let j = 0; j < verticalMatrix[0].length; j++) {
+            if (verticalMatrix[i][j]) frames.push([ j, i, 0 ]);
+            if (horizontalMatrix[i][j]) frames.push([ j, i, 1 ]);
+        }
+    }
+    
+    return frames.sort(([ x1, y1, direction1, ], [ x2, y2, direction2 ]) => x1 - x2 || y1 - y2 || direction1 - direction2);
 }
