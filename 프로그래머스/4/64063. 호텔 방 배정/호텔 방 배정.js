e20a271ce4571280a2237440509f1bb2f399cfeb
Chan <qufstar46@gmail.com>
2025-03-10 19:32:19 +0900

    [level 4] Title: 호텔 방 배정, Time: 372.21 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/4/64063. 호텔 방 배정/호텔 방 배정.js
+++ 프로그래머스/4/64063. 호텔 방 배정/호텔 방 배정.js
@@ -1,79 +1,78 @@
-class UnionFind {
-    constructor(n) {
-        this.parent = {};
-        this.rank = {};
-        this.nextAvailable = {};
+class DisjointSet {
+    constructor() {
+        this.parent = new Map();
+        this.rank = new Map();
+        this.nextAvailable = new Map();
     }
     
-    find(x) {
-        if (!(x in this.parent)) {
-            return null;
+    add(n) {
+        this.parent.set(n, n);
+        this.rank.set(n, 1);
+        this.nextAvailable.set(n, n + 1);
+        if (this.parent.has(n - 1)) {
+            this.union(n - 1, n);
         }
-        if (this.parent[x] !== x) {
-            this.parent[x] = this.find(this.parent[x]);
+        if (this.parent.has(n + 1)) {
+            this.union(n, n + 1);
         }
-        return this.parent[x];
     }
     
-    union(a, b) {
-        let rootA = this.find(a);
-        let rootB = this.find(b);
+    getNextAvailable(n) {
+        const root = this.find(n);
+        return this.nextAvailable.get(root);
+    }
+    
+    find(n) {
+        if (!this.parent.has(n)) return null;
+        if (this.parent.get(n) === n) {
+            return n;
+        }
+        const root = this.find(this.parent.get(n));
+        this.parent.set(n, root);
+        return root;
+    }
+    
+    union(x, y) {
+        const rootX = this.find(x);
+        const rootY = this.find(y);
+        if (rootX === null || rootY === null) return;
+        const rankX = this.rank.get(rootX);
+        const rankY = this.rank.get(rootY);
+        const nextAvail = Math.max(this.nextAvailable.get(rootX), this.nextAvailable.get(rootY));
         
-        if (rootA === null || rootB === null || rootA === rootB) return;
-        
-        if (this.rank[rootA] > this.rank[rootB]) {
-            this.parent[rootB] = rootA;
-            this.nextAvailable[rootA] = Math.max(this.nextAvailable[rootA], this.nextAvailable[rootB]);
+        if (rootX === rootY) return;
+        if (rankX > rankY) {
+            this.parent.set(rootY, rootX);
+            this.nextAvailable.set(rootX, nextAvail);
+        } else if (rankX < rankY) {
+            this.parent.set(rootX, rootY);
+            this.nextAvailable.set(rootY, nextAvail);
         } else {
-            this.parent[rootA] = rootB;
-            this.nextAvailable[rootB] = Math.max(this.nextAvailable[rootA], this.nextAvailable[rootB]);
-            if (this.rank[rootA] === this.rank[rootB]) {
-                this.rank[rootB] += 1;
-            }
-        } 
-    }
-    
-    
-    
-    getNextAvailable(x) {
-        const root = this.find(x);
-        return root !== null ? this.nextAvailable[root] : x;
-    }
-    
-    addAndMerge(x) {
-        if (!(x in this.parent)) {
-            this.parent[x] = x;
-            this.rank[x] = 1;
-            this.nextAvailable[x] = x + 1;
-        }
-        
-        if (x - 1 in this.parent) {
-            this.union(x, x - 1);
-        }
-        
-        if (x + 1 in this.parent) {
-            this.union(x, x + 1);
+            this.parent.set(rootX, rootY);
+            this.nextAvailable.set(rootY, nextAvail);
+            this.rank.set(rootY, rankY + 1);
         }
     }
 }
 
 function solution(k, roomNumbers) {
-    const unionFind = new UnionFind();
-    const assignedRooms = roomNumbers.reduce((accumulator, roomNumber) => {
-        const root = unionFind.find(roomNumber);
+    const disjointSet = new DisjointSet();
+    const answer = [];
+    
+    for (let num of roomNumbers) {
+        const root = disjointSet.find(num);
         
         if (root === null) {
-            accumulator.push(roomNumber);
-            unionFind.addAndMerge(roomNumber);
+            disjointSet.add(num);
+            answer.push(num);
         } else {
-            const newRoom = unionFind.getNextAvailable(roomNumber);
-            accumulator.push(newRoom);
-            unionFind.addAndMerge(newRoom);
+            const nextAvailable = disjointSet.getNextAvailable(root);
+            disjointSet.add(nextAvailable);
+            disjointSet.union(root, disjointSet.find(nextAvailable));
+            answer.push(nextAvailable);
         }
-        return accumulator;
-    }, []);
+        
+    }
     
-    return assignedRooms;
-
-    
+    return answer;
 }
