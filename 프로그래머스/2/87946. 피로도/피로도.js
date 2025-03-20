41ac4ea744a792c552ab820e1a583c4f7de022b5
Chan <qufstar46@gmail.com>
2025-03-21 02:05:48 +0900

    [level 2] Title: 피로도, Time: 13.97 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -1,45 +1,38 @@
-const setDifference = (set1, set2) => new Set([ ...set1 ].filter((element) => !set2.has(element)));
+// 탐색
+// 유저의 현재 피로도, 현재 상태가 방문한 던전을 비트마스킹으로 나타냄
+// 132와 312의 피로도 소모는 같기는 함, 근데 가능 여부가 달라짐
+// 현재 선택한 subtree의 방문이 모두 끝났을 때, 체력을 회복시켜줄 수 있어야 함 => 회복 안 시켜주고 그냥 이전 상태 복원
+// 따라서 필요한 상태는 [ 현재 피로도, 현재 상태가 방문한 던전, 방문 여부 ]
+// 방문 가능한 상태: 8!
 
-function solution(k, dungeons) { 
-    const visitedDungeonIndexSumSet = new Set();
-    visitedDungeonIndexSumSet.add(0);
-    
-    const allDungeonIndexSet = new Set(Array.from({ length: dungeons.length }, (_, index) => index));
-    
-    
+function dfs(maxHp, dungeons) {
     const root = {
-        dungeonIndexSet: new Set(),
-        stamina: k,
+        hp: maxHp,
+        bitmask: 0,
+        dungeonCount: 0,
     };
+    const stack = [];
+    stack.push(root);
     
-    const stack = [ root ];
-    
-    let max = -Infinity;
-    
+    let maxDungeonCount = 0;
     while (stack.length) {
-        const { dungeonIndexSet, stamina } = stack.pop();
-        max = Math.max(max, dungeonIndexSet.size);
+        const { hp, bitmask, dungeonCount } = stack.pop();
+            
+        let bitmaskString = bitmask.toString(2);
         
-        const unvisitedDungeonIndexSet = setDifference(allDungeonIndexSet, dungeonIndexSet);
-        
-        if (unvisitedDungeonIndexSet.size === 0) {
-            return allDungeonIndexSet.size;
-        }
-        
-        const visitedIndexSum = [ ...dungeonIndexSet, ].reduce((accumulator, dungeonIndex) => accumulator + Math.pow(2, dungeonIndex), 0);
-        
-        [ ...unvisitedDungeonIndexSet ].forEach((unvisitedDungeonIndex) => {
-            const newVisitedDungeonIndexSum = visitedIndexSum + Math.pow(2, unvisitedDungeonIndex);
-            const [ requiredStamina, comsumingStamina ] = dungeons[unvisitedDungeonIndex];
-            if (stamina >= requiredStamina && !visitedDungeonIndexSumSet.has(newVisitedDungeonIndexSum)) {
-                visitedDungeonIndexSumSet.add(newVisitedDungeonIndexSum);
-                stack.push({
-                    dungeonIndexSet: new Set(dungeonIndexSet).add(unvisitedDungeonIndex),
-                    stamina: stamina - comsumingStamina,
-                });
-            }
-        });
-    }
-    return max;
+        maxDungeonCount = Math.max(maxDungeonCount, dungeonCount);
 
+        for (let i = 0; i < dungeons.length; i++) {
+            if ((bitmask & (1 << i)) !== 0) continue;
+            const [ requiredHp, spentHp ] = dungeons[i];
+            if (hp < requiredHp || hp < spentHp) continue;
+            stack.push({ hp: hp - spentHp, bitmask: bitmask | (1 << i), dungeonCount: dungeonCount + 1 });
+        }  
+        
+    }
+    return maxDungeonCount;
+}
+
+function solution(k, dungeons) {
+    return dfs(k, dungeons);
 }
