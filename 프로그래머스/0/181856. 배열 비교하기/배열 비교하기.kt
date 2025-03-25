394d12952439762d0a8f25d43b1e3caae1fa645e
Chan <qufstar46@gmail.com>
2025-03-26 00:09:03 +0900

    [level 0] Title: 배열 비교하기, Time: 15.54 ms, Memory: 64.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.kt
@@ -0,0 +1,15 @@
+class Solution {
+    fun solution(arr1: IntArray, arr2: IntArray): Int {
+        return when {
+            arr1.size > arr2.size -> 1
+            arr1.size < arr2.size -> -1
+            else -> {
+                when {
+                    arr1.sum() > arr2.sum() -> 1
+                    arr1.sum() < arr2.sum() -> -1
+                    else -> 0
+                }
+            }
+        }
+    }
+}
