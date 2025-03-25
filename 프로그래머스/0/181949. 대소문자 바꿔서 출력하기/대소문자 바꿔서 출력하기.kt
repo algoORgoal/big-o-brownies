ace32925578b8ab27d352577358e16c563119a80
Chan <qufstar46@gmail.com>
2025-03-25 19:41:59 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 194.49 ms, Memory: 50 MB -BaekjoonHub

--- 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.kt
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.kt
@@ -2,8 +2,8 @@ fun main(args: Array<String>) {
     val str = readLine()!!
     val strArray = str.map {
         when(it) {
-            in 'a'..'z' -> (it.code - 32).toChar()
-            in 'A'..'Z' -> (it.code + 32).toChar()
+            in 'a'..'z' -> (it.code - ('a' - 'A')).toChar()
+            in 'A'..'Z' -> (it.code + ('a' - 'A')).toChar()
             else -> it
         }
     }
