5f7620d1f9f0c1f2f1f268827273714109422afe
Chan <qufstar46@gmail.com>
2025-03-26 12:49:47 +0900

    [level 0] Title: 할 일 목록, Time: 27.63 ms, Memory: 66.9 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.kt
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.kt
@@ -1,12 +1,12 @@
 class Solution {
     fun solution(todo_list: Array<String>, finished: BooleanArray): Array<String> {
-        val entries = todo_list.zip(finished.toList())
+        val entries = todo_list.zip(finished)
         return entries.filter {
-            (task, isFinished) ->
+            [ task, isFinished ] ->
             if (isFinished) false
             else true
         }.map {
-            (task, isFinished) -> task
-        }.toTypedArray()
+            [ task ] -> task
+        }
     }
 }
