65ce6107c5baf001f40a67b805837ede9db337cc
Chan <qufstar46@gmail.com>
2025-03-28 11:41:09 +0900

    [level 0] Title: 배열의 길이를 2의 거듭제곱으로 만들기, Time: 15.96 ms, Memory: 67.1 MB -BaekjoonHub

--- 프로그래머스/0/181857. 배열의 길이를 2의 거듭제곱으로 만들기/배열의 길이를 2의 거듭제곱으로 만들기.kt
+++ 프로그래머스/0/181857. 배열의 길이를 2의 거듭제곱으로 만들기/배열의 길이를 2의 거듭제곱으로 만들기.kt
@@ -1,11 +1,6 @@
-import kotlin.math.log2;
-import kotlin.math.ceil;
-import kotlin.math.pow;
-
 class Solution {
     fun solution(arr: IntArray): IntArray {
-        val exponential = ceil(log2(arr.size.toDouble())).toInt()
-        
-        return arr + IntArray(2.toDouble().pow(exponential).toInt() - arr.size) { 0 }
+        var answer: IntArray = intArrayOf()
+        return answer
     }
 }
