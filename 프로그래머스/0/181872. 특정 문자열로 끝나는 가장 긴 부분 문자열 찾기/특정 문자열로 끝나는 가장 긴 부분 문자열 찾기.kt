91553217031b797408fb976973c9315092d5eb70
Chan <qufstar46@gmail.com>
2025-03-28 14:53:21 +0900

    [level 0] Title: 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기, Time: 20.67 ms, Memory: 63.3 MB -BaekjoonHub

--- 프로그래머스/0/181872. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기/특정 문자열로 끝나는 가장 긴 부분 문자열 찾기.kt
+++ 프로그래머스/0/181872. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기/특정 문자열로 끝나는 가장 긴 부분 문자열 찾기.kt
@@ -1,6 +1,5 @@
 class Solution {
     fun solution(myString: String, pat: String): String {
-        val lastMatchingIndex = myString.windowed(pat.length).indexOfLast { it == pat }
-        return myString.slice(0 until lastMatchingIndex) + pat    
+        return myString.slice(0 until myString.lastIndexOf(pat)) + pat
     }
 }
