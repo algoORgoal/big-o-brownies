15e9f8717b8e5efa713bde7e94a2e9cb4e33d716
Chan <qufstar46@gmail.com>
2025-03-29 20:29:15 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 28.22 ms, Memory: 64.2 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.kt
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.kt
@@ -1,12 +1,20 @@
+import kotlin.math.min
+import kotlin.math.ceil
+
 class Solution {
-    fun getBoxCountToRemove(boxCount: Int, width: Int, current: Int): Int {
-        val next = current + (2 * width) - 1
-        if (next > boxCount) return 1
-        else return 1 + getBoxCountToRemove(boxCount, width, next)
-    }
-    
-    
     fun solution(n: Int, w: Int, num: Int): Int {
-        return getBoxCountToRemove(n, w, num)
+        val boxList = (1..n)
+            .chunked(w)
+            .mapIndexed { index, row ->
+                val missingCount = w - row.size
+                when (index % 2) {
+                    0 -> row + List(missingCount) { 0 }
+                    1 -> List(missingCount) { 0 } + row.reversed()
+                    else -> throw Exception("integer not valid")
+                }
+            }
+            val x = boxList.indexOfFirst { num in it }
+            val y = boxList[x].indexOf(num)
+            return boxList.drop(x).count { it[y] != 0 }
     }
 }
