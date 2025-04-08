69cac2bbf25b129afb6f7e0a09f84582e1edb405
Chan <qufstar46@gmail.com>
2025-04-08 19:56:27 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.kt
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.kt
@@ -1,17 +1,6 @@
 class Solution {
-//     tailrec fun getPalindrome(foods: List<Int>, acc: String, current: Int):  {
-        
-//     }
-    fun solution(foods: IntArray): String {
-        return foods.reversed().foldIndexed("0") { index, acc, count ->
-            val type = foods.size - index - 1
-            when {
-                index == foods.size - 1 -> acc
-                else -> {
-                    val round = type.toString().repeat(count / 2)
-                    round + acc + round
-                }
-            }
-        }
+    fun solution(food: IntArray): String {
+        var answer: String = ""
+        return answer
     }
 }
