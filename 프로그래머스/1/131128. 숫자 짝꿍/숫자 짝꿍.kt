56bcae444db37ced8a062b4a9a4a72acfa5469d5
Chan <qufstar46@gmail.com>
2025-04-13 18:10:08 +0900

    [level 1] Title: 숫자 짝꿍, Time: 255.23 ms, Memory: 152 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.kt
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.kt
@@ -1,24 +1,6 @@
-import kotlin.math.min
-
 class Solution {
     fun solution(X: String, Y: String): String {
-        val xMap = X.fold(mutableMapOf<Char, Int>()) { acc, char ->
-            acc[char] = (acc[char] ?: 0) + 1
-            acc
-        }.toSortedMap(Comparator.reverseOrder())
-        val yMap = Y.fold(mutableMapOf<Char, Int>()) { acc, char ->
-            acc[char] = (acc[char] ?: 0) + 1
-            acc
-        }.toSortedMap(Comparator.reverseOrder())
-        val commonKeys = xMap.keys.intersect(yMap.keys)
-        val commonPairs = commonKeys.associateWith { key -> min(xMap[key]!!, yMap[key]!!) }
-        val string = commonPairs.toList().fold("") { acc, (char, count) -> acc + char.toString().repeat(count) }
-        return when {
-            string.length == 0 -> "-1"
-            string.startsWith("0") -> "0"
-            else -> string
-        }
-        
-        
+        var answer: String = ""
+        return answer
     }
 }
