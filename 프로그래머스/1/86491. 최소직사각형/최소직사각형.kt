c0bbe8466f25d3fe85428a4528648384c549fe31
Chan <qufstar46@gmail.com>
2025-04-19 18:49:55 +0900

    [level 1] Title: 최소직사각형, Time: 52.05 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.kt
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.kt
@@ -1,8 +1,6 @@
 class Solution {
     fun solution(sizes: Array<IntArray>): Int {
-        val sortedSizes = sizes.map { it.toList().sorted() }
-        val maxWidth = sortedSizes.maxOf { it.first() }
-        val maxHeight = sortedSizes.maxOf { it.last() }
-        return maxWidth * maxHeight
+        var answer: Int = 0
+        return answer
     }
 }
