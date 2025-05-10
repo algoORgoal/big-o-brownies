b4c5d6d47a125afe8ca497e513b73147160a1d62
Chan <qufstar46@gmail.com>
2025-05-10 15:45:59 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.kt
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.kt
@@ -1,12 +1,6 @@
 class Solution {
     fun solution(arr: IntArray): IntArray {
-        return arr.withIndex().minByOrNull { (index, element) -> element }?.let { (minIndex, minElement) ->
-            (arr.slice(0 until minIndex) + arr.slice(minIndex + 1 until arr.size)).toIntArray() 
-        }?.let {            
-            when (it.size) {
-                0 -> listOf(-1).toIntArray()
-                else -> it
-            }
-        } ?: listOf(-1).toIntArray()
+        println(arr.toList().min())
+        return listOf(0).toIntArray()
     }
 }
