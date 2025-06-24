b131254a4b94f60d0ac36e83eda1b7ca0e7b372b
Chan <qufstar46@gmail.com>
2025-06-25 01:27:54 +0900

    [level 2] Title: 하노이의 탑, Time: 45.42 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.kt
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.kt
@@ -1,18 +1,6 @@
-// f(1, 3, n)
-// = f(1, 2, n - 1) + [1, 3]
-//   + f(2, 1, n - 1) + [2, 3]
-//   + f(1, 2, n - 2) + [1, 3]
-
 class Solution {
-    fun getOrder(source: Int, other: Int, destination: Int, count: Int): List<Pair<Int, Int>> {
-        return when (count) {
-            1 -> listOf(source to destination)
-            else -> getOrder(source, destination, other, count - 1) + listOf(source to destination) + getOrder(other, source, destination, count - 1)
-        }
-    }
-    
     fun solution(n: Int): Array<IntArray> {
-        val order = getOrder(1, 2, 3, n)
-        return order.map { (from, to) -> listOf(from, to).toIntArray() }.toTypedArray()
+        var answer = arrayOf<IntArray>()
+        return answer
     }
 }
