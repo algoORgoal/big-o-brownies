a55b9a2fabcc169f5fca52f899ccdb111b0ff9a9
Chan <qufstar46@gmail.com>
2025-07-07 05:01:27 +0900

    [level 2] Title: 문자열 압축, Time: 43.76 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.kt
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.kt
@@ -1,36 +1,6 @@
-
-
 class Solution {
-    fun compressString(s: String, length: Int): String {
-        var i = 0;
-        var newString = ""
-        while (i < s.length) {
-            var j = i + length
-            if (j + length <= s.length && s.slice(j until j + length) == s.slice(i until i + length)) {
-                val word = s.slice(i until i + length)
-                var wordCount = 1
-                while (j + length <= s.length && s.slice(j until j + length) == word) { 
-                    j += length 
-                    wordCount +=1
-                }
-                newString += (wordCount.toString() + word)
-                i = j
-            } else {
-                if (i + length > s.length) {
-                    newString += s.slice(i until s.length)
-                    break
-                }
-                newString += s.slice(i until i + length)
-                i += length
-            }
-        }
-        
-        
-        return newString
-    }
-    
     fun solution(s: String): Int {
-        return (1..s.length).map { compressString(s, it).length }.toList().minOrNull() ?: 0   
+        var answer = 0
+        return answer
     }
 }
-
