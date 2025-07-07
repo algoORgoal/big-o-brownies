fe08191bb75327ebdfa4f779ecdc42aa8ab5bc6d
Chan <qufstar46@gmail.com>
2025-07-08 01:31:58 +0900

    [level 2] Title: 비밀 코드 해독, Time: 443.83 ms, Memory: 223 MB -BaekjoonHub

--- 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.kt
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.kt
@@ -1,6 +1,24 @@
 class Solution {
+    fun combination(n: Int, k: Int, current: Set<Int>, max: Int): List<Set<Int>> {
+        if (current.size == k) return listOf(current)
+        val range = if (current.isEmpty()) 1..n else max+1..n
+        return range.fold(emptyList()) { acc, element ->
+            if (element in current) acc
+            else acc + combination(n, k, current + element, element)
+        }
+    }
+    
+    
     fun solution(n: Int, q: Array<IntArray>, ans: IntArray): Int {
-        var answer: Int = 0
-        return answer
+        val lists = combination(n, 5, emptySet(), 0)
+        
+        return lists.filter { set -> 
+            q.withIndex().all { (index, query) ->
+                query.filter { number -> number in set }.size == ans[index]
+            } 
+        }.size
+        return 0
     }
 }
+
+// 30C5 = 142506
