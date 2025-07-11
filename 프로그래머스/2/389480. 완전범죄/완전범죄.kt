36dcaa80b1a852b6c2088b32ddadf9a8dab4602c
Chan <qufstar46@gmail.com>
2025-07-11 16:28:38 +0900

    [level 2] Title: 완전범죄, Time: 90.35 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.kt
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.kt
@@ -8,31 +8,24 @@ class Solution {
         }.toMutableList<MutableList<Int>>()
         
         info.map { it[0] to it[1] }.forEach { (a, b) ->
-            val temp1 = (0..n).map {
-                (0..m).map { 0 }.toMutableList()
-            }.toMutableList<MutableList<Int>>()
+            val updates = mutableMapOf<Pair<Int, Int>, Int>()
             
-            val temp2 = (0..n).map {
-                (0..m).map { 0 }.toMutableList()
-            }.toMutableList<MutableList<Int>>()
-
             for (i in 0..n) {
                 for (j in 0..m) {
                     if (dp[i][j] >= 1 || (i == 0 && j == 0)) {
                         if (i + a <= n) {
-                            temp1[i + a][j] = max(dp[i + a][j], dp[i][j] + 1)
+                            updates[i + a to j] = max(updates[i + a to j] ?: 0, dp[i][j] + 1)
                         }
                         if (j + b <= m) {
-                            temp2[i][j + b] = max(dp[i][j + b], dp[i][j] + 1)
+                            updates[i to j + b] = max(updates[i to j + b] ?: 0, dp[i][j] + 1)
                         }      
                     }  
                 }   
             }
             
-            for (i in 0..n) {
-                for (j in 0..m) {
-                    dp[i][j] = listOf(temp1[i][j], temp2[i][j], dp[i][j]).maxOrNull() ?: 0
-                }
+            updates.entries.map { (pair, value) -> 
+                val (a, b) = pair
+                dp[a][b] = max(dp[a][b], value)
             }
         }
         
@@ -53,3 +46,4 @@ class Solution {
 
 // f(a, b): A 도둑이 a개 흔적, b 도둑이 b개 흔적을 남길 때 훔친 물건의 인덱스 목록을 저장하면 되지 않나?
 // f(a + info[i][0], b + info[i][1]) = f(a, b) 
+// 단, 흔적은 도둑질을 할 수 있는 곳에만 남길 수 있다. f(a, b) >= 1 아니면 a = 0, b = 0이어야 함.
