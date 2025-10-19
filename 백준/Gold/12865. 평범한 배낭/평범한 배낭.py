312bf47d7047bbdabf84d2d4676463ee0c3b03bd
Chan <qufstar46@gmail.com>
2025-10-19 16:35:19 +0900

    [Gold V] Title: 평범한 배낭, Time: 1272 ms, Memory: 36264 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,6 +1,9 @@
 # dp[j] = max(dp[j], dp[j - w] + v) (j >= w), dp[0] = 0
 # 문제였던 부분: 각 dp마다 모든 물건의 가치를 고려한 최대합을 계산하려고 했으나, 순차적으로 각 물건을 고려했을 때의 최댓값을 계산해야 문제를 풀 수 있다.
 
+# 시간복잡도: O(k * len(array))
+# 공간복잡도: O(k)
+
 def main(n, k, stuffs):
     dp = [ 0 for i in range(k + 1) ]
     for weight, value in stuffs:
