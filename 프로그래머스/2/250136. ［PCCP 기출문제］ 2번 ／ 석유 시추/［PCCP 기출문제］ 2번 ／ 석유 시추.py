b2d7c11a9e18ca9110fd51e56375dee98905e3a1
Chan <qufstar46@gmail.com>
2026-02-09 19:15:48 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 879.81 ms, Memory: 223 MB -BaekjoonHub

---
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
@@ -0,0 +1,74 @@
+import sys
+sys.setrecursionlimit(10**7)
+
+
+
+def solution(land):
+    visited = [ [ False for j in range(len(land[0])) ] for i in range(len(land))]
+    rowCount = len(land)
+    colCount = len(land[0])
+    
+    for i in range(0, rowCount):
+        for j in range(0, colCount):
+            if visited[i][j] == False and land[i][j] > 0:
+                chunk = []
+                dfs(land, i, j, visited, chunk, (i, j))
+                for chunkX, chunkY in chunk:
+                    land[chunkX][chunkY] = land[i][j]
+                    
+                    
+    
+    
+    
+    maxSum = 0
+    for j in range(0, colCount):
+        sum = 0
+        columnSet = set()
+        for i in range(0, rowCount):
+            if land[i][j] != 0:
+                id = visited[i][j]
+                if id not in columnSet:
+                    columnSet.add(id)
+                    sum += land[i][j]
+            
+        maxSum = max(sum, maxSum)
+        
+    return maxSum
+    
+                
+    
+                
+                
+def dfs(land, x, y, visited, chunk, id):
+    visited[x][y] = id
+    chunk.append([x, y])
+    
+    rowCount = len(land)
+    colCount = len(land[0])
+    
+    next = [[ x - 1, y ], [ x + 1, y ], [ x, y - 1], [ x, y + 1 ]]
+    
+    total = 0
+    
+    for nextX, nextY in next:
+        if 0 <= nextX < rowCount and 0 <= nextY < colCount:
+            if visited[nextX][nextY] == False and land[nextX][nextY] > 0:
+                dfs(land, nextX, nextY, visited, chunk, id)
+                total += land[nextX][nextY]
+        
+    land[x][y] = total + land[x][y]
+    
+    
+# 못찾은 반례: [ 1 1 1] [1 0 1] [ 1 1 1 ]
+# 가운데 뚫려있는 것
+    
+    
+    
+    
+
+# 1. 각 칸에서 추출할 수 있는 석유량을 열 시추시 매번 구함 => O(n ** 2 * m ** 2) => 시간초과
+# 2. 미리 각 칸에서 추출할 수 있는 석유량을 구해서 칸에 저장 후, 열의 시추량 구할 때는 다음과 같이 석유량 합 구함
+#    - 첫 행이거나, 그 윗칸이 석유 없는 칸인 경우 합에 포함
+#    dfs를 돌리고 돌아왔을 때 누적합을 더해줌, 이 값이 석유량
+#    모든 석유 덩어리에 대해서 석유량 값을 저장
+#    시간복잡도 O(n * m), 공간복잡도 O(n * m)
