6c1fe54987c9774929336769bb2d700a1666a7b6
Chan <qufstar46@gmail.com>
2026-02-14 01:48:55 +0900

    [level 3] Title: 이중우선순위큐, Time: 113.60 ms, Memory: 23.3 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/README.md
+++ 프로그래머스/3/42628. 이중우선순위큐/README.md
@@ -1,10 +1,10 @@
 # [level 3] 이중우선순위큐 - 42628 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42628#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42628?language=python3) 
 
 ### 성능 요약
 
-메모리: 53.1 MB, 시간: 275.45 ms
+메모리: 23.3 MB, 시간: 113.60 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 05일 03:13:22
+2026년 02월 14일 01:48:53
 
 ### 문제 설명
 
---
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -0,0 +1,58 @@
+import heapq
+import sys
+from math import inf
+
+
+min_queue = []
+max_queue = []
+deletion_history = set()
+
+def heappush(value, id):
+    heapq.heappush(max_queue, [-value, id])
+    heapq.heappush(min_queue, [value, id])
+    
+
+def heappop_max():    
+    while len(max_queue) > 0:
+        inverted_value, id = heapq.heappop(max_queue)
+        if id not in deletion_history:
+            deletion_history.add(id)
+            return -inverted_value
+    return None
+        
+def heappop_min():    
+    while len(min_queue) > 0:
+        value, id = heapq.heappop(min_queue)
+        if id not in deletion_history:
+            deletion_history.add(id)
+            return value
+    return None
+
+
+    
+    
+
+
+def solution(operations):    
+    for index, operation in enumerate(operations):
+        command, numStr = operation.split(' ')
+        num = int(numStr)
+        if command == "I":
+            heappush(num, index)
+        elif command == "D" and num == 1 and len(max_queue) > 0:
+            heappop_max()
+        elif command == "D" and num == -1 and len(min_queue) > 0:
+            heappop_min()
+    
+    max_value = heappop_max() or 0
+    min_value = heappop_min() or max_value or 0
+    return [ max_value, min_value ]
+    
+    
+    
+
+# min_queue와 max_queue를 별도로 운영
+# min_queue에서 삭제된 요소를 max_queue에서 접근하지 않도록 id 같이 저장
+# min_queue에서 삭제된 요소를 max_queue에서 접근하지 않도록 id 같이 저장
+        
+    
