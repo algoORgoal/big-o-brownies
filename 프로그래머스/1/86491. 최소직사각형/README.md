69d252419afd2a11d8f66e34ea0f0e803753e75f
Chan <qufstar46@gmail.com>
2026-02-14 22:50:36 +0900

    [level 1] Title: 최소직사각형, Time: 4.71 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/README.md
+++ 프로그래머스/1/86491. 최소직사각형/README.md
@@ -1,10 +1,10 @@
 # [level 1] 최소직사각형 - 86491 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 2.17 ms
+메모리: 10 MB, 시간: 4.71 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 14일 22:37:27
+2026년 02월 14일 22:50:35
 
 ### 문제 설명
 
--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -1,9 +1,17 @@
 def solution(sizes):
+    max_width = 0
+    max_height = 0
     for i in range(0, len(sizes)):
         width, height = sizes[i]
-        if width < height:
-            sizes[i] = [height, width]
-    return max(size[0] for size in sizes) * max(size[1] for size in sizes)
+        if width > height:
+            max_width = max(width, max_width)
+            max_height = max(height, max_height)
+        else:
+            max_width = max(height, max_width)
+            max_height = max(width, max_height)
+    return max_width * max_height
+            
+    
     
     
 
@@ -12,9 +20,14 @@ def solution(sizes):
 
 # 최소로 만드는 방법:
 # 모든 도형의 가로를 길게, 세로를 짧게
-# max_a, max_b
+# max_a * max_b
 # 이 때 a[i], b[i]를 스위칭한다고 해보자.
-# a[i] > b[i] 이므로 max_b가 더 커질 수 있다.
+# 1. max_b를 스위칭하는 경우: 무조건 커짐
+# 2. max_a를 스위칭하는 경우: 무조건 커짐
+# 3. 아닌 경우: 동일 값 유지
+
+# max_a >= a[i] >= min_a, max_a > max_b >= b[i] >= min_b
+# max_a == a[i] 인 경우 
 
 
 
