d47b6cf37d0fe382228b0878fcbb2598d8f1b3cf
Chan <qufstar46@gmail.com>
2026-02-21 18:52:24 +0900

    [level 4] Title: 징검다리, Time: 350.69 ms, Memory: 11.9 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.py
+++ 프로그래머스/4/43236. 징검다리/징검다리.py
@@ -1,56 +1,72 @@
 from math import ceil
 
 def solution(distance, rocks, n):
-    sorted_rocks = [ 0 ] + sorted(rocks) + [ distance ]
+    rocks.sort()
+    
+    in_between_distances = []
+    for i, rock in enumerate(rocks):
+        if i == 0: 
+            in_between_distances.append(rock)
+        else:
+            in_between_distances.append(rocks[i] - rocks[i - 1])
+    in_between_distances.append(distance - rocks[len(rocks) - 1])
+    
+    
     
     start = 0
     end = 1_000_000_000
+        
     while start < end:
         middle = ceil((start + end) / 2)
         
-        target_distance = middle
         
-        count = 0
-        start_pointer = 0
-        end_pointer = 1
-        
-        while end_pointer < len(sorted_rocks):
-            distance = sorted_rocks[end_pointer] - sorted_rocks[start_pointer]
-            if distance < target_distance:
-                end_pointer += 1
-                count += 1
+        minimum_distance = middle
+        current_distance = in_between_distances[0]
+        destroy_count = 0
+
+        for i, in_between_distance in enumerate(in_between_distances):
+            if i == 0:
+                continue
+            elif current_distance < minimum_distance:
+                destroy_count += 1
+                current_distance += in_between_distance
             else:
-                end_pointer += 1
-                start_pointer = end_pointer - 1
-        
-        if count <= n:
+                current_distance = in_between_distance
+        if current_distance < minimum_distance:
+            destroy_count += 1
+                        
+        # 거리 i 유지하려면 바위 n개 제거로 가능 => 거리 0 ~ (i - 1) 거리 유지하려면 바위 n개 제거로 가능
+        # 거리 i 유지하려면 바위 n개 제거로 불가능 => 거리 (i + 1) ~ n 거리 유지하려면 바위 n개 제거로 불가능
+        if destroy_count <= n:
             start = middle
         else:
             end = middle - 1
-        
     return start
-            
-        
-        
-        
-        
     
 
-# 바위들 n개를 제거한 뒤 지점 사이의 거리의 최솟값 중에 가장 큰 값
-# == 바위들 n개를 제거한 뒤 유지 가능한 거리 최댓값
+            
+    
+        
+    
+    
+    answer = 0
+    return answer
 
-# 바위 n개 제거 모든 경우의 수 => 50_000Cn
-# 최댓값 => 50_000
-# 시간복잡도 50_000Cn * 50_000 => 너무 큼
 
-# 바위들 n개를 제거한 뒤 유지 가능한 거리 최댓값
-# == 유지 거리 d (1 <= d <= 1_000_000_000) 중, 바위 n개 제거로 만들 수 있는 d 중 최대값
-# 유지 거리 d에 필요한 바위 제거 횟수 n => 유지 거리 d + 1 에 필요한 바위 제거 횟수 n 이하
-# 유지 거리 d를 바위 제거 횟수 n 이하로 만들 수 있음 => 탐색 범위 [d, 1_000_000_000]
-# 유지 거리 d가 바위 제거 횟수 n 이하로 만들 수 없음 => 탐색 범위 [1, d - 1]
+# 0 2 11 14 17 21 25
+#  2 9  3 3  4  4
 
-# distance: n, rocks: m
-# 이진탐색 => O(logn)
-# 최소 바위 제거 횟수 => O(m)
+# 현재 값 2
+# 9 들어올 때 2 <= d => destory => 2 + 9 = 11
+# 11 들어올 때 11 >= d => replace => 현재값 9
+# 14 들어올 때 9 >= d => replace => 현재값 3
+# 17 들어올 때 3 < d => destroy => 3 + 3 = 6
+# 21 들어올 때 4 >= d => replace => 현재값 4
+# 25 들어올 때 4 >= d => replace => 현재값 4
 
-# O(log n * m) (n <= 1_000_000_000, m <= 50_000)
+
+# 질문: 특정 거리가 들어왔을 때, 왼쪽에서 destroy해야될까 아니면 오른쪽에서 destroy해야될까?
+
+# 거리 d를 유지하기 위해 제거해야되는 요소들의 최소 개수를 어떻게 구하지?
+# 가장 작은 것부터 순차적으로 배열 탐색하면서 제거: O(n ** 2) 2_500_000_000 => 시간초과
+
