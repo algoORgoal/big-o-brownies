649b1dd19b976ba5db6d5e1b1f8d8393ec430b71
Chan <qufstar46@gmail.com>
2026-03-01 03:12:21 +0900

    [level 3] Title: 순위, Time: 29.70 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.py
+++ 프로그래머스/3/49191. 순위/순위.py
@@ -16,13 +16,6 @@ def solution(n, results):
     parent_table = get_children_table(reversed_graph, nodes)
     
     
-    # 한번 visited된건 다시 visited하면 안 됌.
-    # 예: 4 => 3 => 2, 4 => 2 둘 다 가능
-    # visited 유지할 경우 조상 개수는 업데이트해주기 힘듦. 왜냐하면 매번 조상 방문했을 때 visited된 후손까지 업데이트해줘야 하기 때문
-    # 후손 개수만 제대로 세보자
-    # 근데 후손 개수 셀때도 4는 3, 2 중에 3만 골라야 함
-    # 해답: 3이 reachable하는 노드 set, 2에서 recahble하는 노드 set 별도로 가지고 있자.
-    
     count = 0
     for node in nodes:
         if len(children_table[node]) + len(parent_table[node]) == n - 1:
@@ -46,21 +39,18 @@ def get_children_table(graph, nodes):
     
 
 def dfs(graph, current, visited, children_table):
-    if current in graph:
-        adjacent_nodes = graph[current]
+    children_table[current] = set()
+    if current not in graph:
+        return
 
-        children_table[current] = set()
-        for adjacent_node in adjacent_nodes:
-            if adjacent_node in visited:
-                children_table[current] = children_table[current] | set([ adjacent_node ])
-                children_table[current] = children_table[current] | children_table[adjacent_node]
-                continue
-            visited.add(adjacent_node)
+    adjacent_nodes = graph[current]
+    for adjacent_node in adjacent_nodes:
+        if adjacent_node not in visited:
             dfs(graph, adjacent_node, visited, children_table)
-            children_table[current] = children_table[current] | set([ adjacent_node ])
-            children_table[current] = children_table[current] | children_table[adjacent_node]
-    else:
-        children_table[current] = set()
+        visited.add(adjacent_node)
+        children_table[current] = children_table[current] | set([ adjacent_node ])
+        children_table[current] = children_table[current] | children_table[adjacent_node]
+    
         
         
         
@@ -82,3 +72,11 @@ def dfs(graph, current, visited, children_table):
 # 1 => 2
 # if 앞에 있는 노드 개수 + 뒤에 있는 노드 == n - 1
 #   count += 1
+
+
+# 한번 visited된건 다시 visited하면 안 됌.
+# 예: 4 => 3 => 2, 4 => 2 둘 다 가능
+# visited 유지할 경우 조상 개수는 업데이트해주기 힘듦. 왜냐하면 매번 조상 방문했을 때 visited된 후손까지 업데이트해줘야 하기 때문
+# 후손 개수만 제대로 세보자
+# 근데 후손 개수 셀때도 4는 3, 2 중에 3만 골라야 함
+# 해답: 3이 reachable하는 노드 set, 2에서 recahble하는 노드 set 별도로 가지고 있자.
