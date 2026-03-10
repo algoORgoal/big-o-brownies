13cc76b16b067db6187a79d5e72a4c02b682a657
Chan <qufstar46@gmail.com>
2026-03-11 04:09:46 +0900

    [Gold V] Title: 로봇 청소기, Time: 44 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -0,0 +1,71 @@
+
+def solution(size, initial_point, d, matrix):
+    n, m = size
+    point = initial_point
+    direction = d
+
+    count = 0
+    while True:
+        x, y = point
+        if matrix[x][y] == 0:
+            count += 1
+            matrix[x][y] = 2
+
+        candidates = (x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)
+
+        if any([0 <= candidate_x < n and 0 <= candidate_y < m and matrix[candidate_x][candidate_y] == 0 for candidate_x, candidate_y in candidates]):
+            direction = (direction - 1) if direction > 0 else 3
+            next_x, next_y = resolve_next_point(point, direction)
+
+            if 0 <= next_x < n and 0 <= next_y < m and matrix[next_x][next_y] == 0:
+                point = next_x, next_y
+        else:
+            back_x, back_y = resolve_back_point(point, direction)
+            if 0 <= back_x < n and 0 <= back_y < m and matrix[back_x][back_y] != 1:
+                point = back_x, back_y
+            else:
+                break
+
+    return count
+
+
+def resolve_next_point(point, direction):
+    x, y = point
+    # 북 동 남 서
+    if direction == 0:
+        return x - 1, y
+    if direction == 1:
+        return x, y + 1
+    if direction == 2:
+        return x + 1, y
+    else:
+        return x, y - 1
+
+
+def resolve_back_point(point, direction):
+    x, y = point
+    # 북 동 남 서
+    if direction == 0:
+        return x + 1, y
+    if direction == 1:
+        return x, y - 1
+    if direction == 2:
+        return x - 1, y
+    else:
+        return x, y + 1
+
+
+if __name__ == "__main__":
+    n, m = [int(string) for string in input().split()]
+    r, c, d = [int(string) for string in input().split()]
+    matrix = [[int(string) for string in input().split()] for i in range(n)]
+    answer = solution((n, m), (r, c), d, matrix)
+    print(answer)
+
+
+# 상태
+# 현재 청소기의 좌표 (x, y)
+# 현재 청소기의 방향 d
+
+# 시간복잡도 O(nm)
+# 공간복잡도 O(nm)
