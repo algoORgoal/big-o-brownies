8a2587600db9111d65a53517c55901632863e75a
Chan <qufstar46@gmail.com>
2026-04-04 00:19:58 +0900

    [Silver IV] Title: 어두운 굴다리, Time: 216 ms, Memory: 44788 KB -BaekjoonHub

---
+++ 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.py
@@ -0,0 +1,55 @@
+from math import ceil
+
+
+def solution(n, m, street_lamps):
+    start = 1
+    end = 100_000
+
+    while start < end:
+        mid = (start + end) // 2
+
+        is_valid = True
+
+        first = street_lamps[0]
+        last = street_lamps[len(street_lamps) - 1]
+
+        left_distance = first
+        right_distance = n - last
+
+        if left_distance > mid:
+            is_valid = False
+        elif right_distance > mid:
+            is_valid = False
+        else:
+            for i in range(len(street_lamps) - 1):
+                first, second = street_lamps[i], street_lamps[i + 1]
+                distance = second - first
+                if distance > 2 * mid:
+                    is_valid = False
+
+        if is_valid == True:  # 가로등의 높이 h에서 ok => 더 낮춰봐도 됌
+            end = mid
+        else:
+            start = mid + 1  # 가로등의 높이 h에서 not ok => 더 높여야 됌
+
+    return start
+
+
+if __name__ == "__main__":
+    n = int(input())
+    m = int(input())
+    street_lamps = [int(string) for string in input().split()]
+    answer = solution(n, m, street_lamps)
+    print(answer)
+
+
+# 모든 가로등 사이의 차이가 2 * h 이하인지 확인하기
+# 왼쪽 거리 - 가로등 거리가 h 이하, 가로등 - 오른쪽 거리 거리가 h이하
+
+# O(nm) <= O(n ** 2) 10_000_000_000 => 시간 초과
+# 이진탐색 활용
+# 높이 h일 때 True => 높이 h + 1일 때 True
+# True가 되는 최솟값 찾기
+
+
+# 3
