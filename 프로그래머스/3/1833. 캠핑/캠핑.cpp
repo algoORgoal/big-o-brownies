b60a921dd024ebfcff2802260afa43995463761d
Chan <qufstar46@gmail.com>
2026-06-17 15:00:38 +0900

    [level 3] Title: 캠핑, Time: 1316.14 ms, Memory: 100 MB -BaekjoonHub

--- 프로그래머스/3/1833. 캠핑/캠핑.cpp
+++ 프로그래머스/3/1833. 캠핑/캠핑.cpp
@@ -1,110 +1,130 @@
-#include <vector>
 #include <iostream>
-#include <cmath>
-#include <unordered_map>
+#include <vector>
 #include <algorithm>
-#include <unordered_set>
+#include <unordered_map>
+#include <set>
 
 using namespace std;
 
-// 전역 변수를 정의할 경우 함수 내에 초기화 코드를 꼭 작성해주세요.
 int solution(int n, vector<vector<int>> data) {
-    unordered_set<pair<pair<int, int>, pair<int, int>>> visited;
-    unordered_map<int, vector<int>> x_to_y_list;
+    vector<int> x_list;
+    vector<int> y_list;
     
     for (vector<int> position: data) {
-        auto x = position[0], y = position[1];
-        x_to_y_list[x].push_back(y);
+        int x = position[0], y = position[1];
+        x_list.push_back(x);
+        y_list.push_back(y);
     }
     
-    unordered_set<int> x_keys;
-    for (auto [x, y_list]: x_to_y_list) {
-        x_keys.insert(x);
+    sort(x_list.begin(), x_list.end());
+    sort(y_list.begin(), y_list.end());
+    
+    x_list.erase(unique(x_list.begin(), x_list.end()), x_list.end());
+    y_list.erase(unique(y_list.begin(), y_list.end()), y_list.end());
+    
+    
+    unordered_map<int, int> x_to_index;
+    unordered_map<int, int> y_to_index;
+    
+    for (int i = 0; i < x_list.size(); i++) {
+        x_to_index[x_list[i]] = i;
     }
     
+    for (int i = 0; i < y_list.size(); i++) {
+        y_to_index[y_list[i]] = i;
+    }
     
-    vector<int> x_key_vector(x_keys.begin(), x_keys.end());
-    x_key_vector.sort(x_key_vector.begin(), x_key_vector.end());
-        
+    set<pair<int, int>> compressed_points;
+    
+    for (vector<int> position: data) {
+        int x = position[0], y = position[1];
+        int compressed_x = x_to_index[x], compressed_y = y_to_index[y];
+        pair<int, int> compressed_point(compressed_x, compressed_y);
+        compressed_points.insert(compressed_point);
+    }
+    
+    vector<vector<int>> count_point(n, vector<int>(n));
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i - 1 >= 0 && j - 1 >= 0) {
+                count_point[i][j] = count_point[i - 1][j] + count_point[i][j - 1] - count_point[i - 1][j - 1];
+            }
+            else if (i - 1 >= 0) {
+                count_point[i][j] = count_point[i - 1][j];
+            }
+            else if (j - 1 >= 0) {
+                count_point[i][j] = count_point[i][j - 1];
+            }
+            
+            pair<int, int> p = { i, j };
+            
+            if (compressed_points.find(p) != compressed_points.end()) {
+                count_point[i][j] += 1;
+            }
+        }
+    }
+    
+    vector<pair<int, int>> compressed_point_vector(compressed_points.begin(), compressed_points.end());
+    
     int total = 0;
     
-    for (int i = 0; i < x_key_vector.size(); i++) {
-        if (i == 0) {
-            continue;
-        }
-        
-        int last_x = x_key_vector[i - 1];
-        int current_x = x_key_vector[i];
-        
-        for (int last_y: x_to_y_list[last_x]) {
-            for (int current_y: x_to_y_list[current_x]) {
-                visited.insert(
-                    minmax(
-                        pair<int, int>(last_x, last_y),
-                        pair<int, int>(current_x, current_y)
-                    )
-                );
-
+    for (int i = 0; i < compressed_point_vector.size(); i++) {
+        for (int j = i + 1; j < compressed_point_vector.size(); j++) {
+            pair<int, int> x_pair = minmax(compressed_point_vector[i].first, compressed_point_vector[j].first);
+            pair<int, int> y_pair = minmax(compressed_point_vector[i].second, compressed_point_vector[j].second);
+            
+            int min_x = x_pair.first, max_x = x_pair.second;
+            int min_y = y_pair.first, max_y = y_pair.second;
+            
+            if (min_x == max_x || min_y == max_y) continue;
+            
+            if (max_x - min_x <= 1 || max_y - min_y <= 1) {
+                total += 1;
+                continue;
+            }
+            
+            
+            int count = count_point[max_x - 1][max_y - 1];
+            if (min_x >= 0) count -= count_point[min_x][max_y - 1];
+            if (min_y >= 0) count -= count_point[max_x - 1][min_y];
+            if (min_x >= 0 && min_y >= 0) count += count_point[min_x][min_y];    
+            
+            if (count == 0) {
                 total += 1;
             }
         }
     }
     
-    
-    unordered_map<int, vector<int>> y_to_x_list;
-
-    
-    for (vector<int> position: data) {
-        auto x = position[0], y = position[1];
-        y_to_x_list[y].push_back(x);
-    }
-    
-    unordered_set<int> y_keys;
-    for (auto [y, x_list]: y_to_x_list) {
-        y_keys.insert(y);
-    }
-    
-    
-    vector<int> y_key_vector(y_keys.begin(), y_keys.end());
-    y_key_vector.sort(y_key_vector.begin(), y_key_vector.end());
-    
-    for (int i = 0; i < y_key_vector.size(); i++) {
-        if (i == 0) {
-            continue;
-        }
-        
-        int last_y = y_key_vector[i - 1];
-        int current_y = y_key_vector[i];
-        
-        for (int last_x: y_to_x_list[last_y]) {
-            for (int current_x: y_to_x_list[current_y]) {
-                    if (!visited.contains(
-                        minmax(
-                            pair<int, int>(last_x, last_y),
-                            pair<int, int>(current_x, current_y),
-                        )
-                    )) {
-                        total += 1;
-                    }
-                }
-            }
-        }
-    }
-
     return total;
+    
+
+    
+    
+    
+    
+    
+    return 0;
+    
+    
+    
+    
+    // unordered_map<int, int> x_to_index;
+    // unordered_map<int, int> y_to_index;
+    
+    
 }
 
 
-// O(n) 시간 안에 스위핑으로 풀 수 있을 듯
-// 1. x 좌표: y좌표 리스트 형태로 변환
-// 2. x좌표 순으로 정렬하여 순회
-//      count += 이전 x좌표 점의 개수 * 현재 x좌표 점의 개수(경계에 위치하는 경우 허용, 다른 점 포함하면 안 됌)
-// 
-
-// 반례: (1, 1), (2, 3), (3, 2)
-// 1 * 1 + 1 * 1 = 2이지만,
-// (1, 1), (3, 2)로도 가능하다. => y좌표 기준으로도 정렬을 해야 된다.
-// 그리고 x좌표에서, 이미 지나간 쌍인지 확인도 해줘야 된다.
-
-
-// 25_000_000
+// 1. 값을 인덱스로 변환할 수 있는 x table, y table 만들기 => 2 ** 31 에서 5000 size
+// 2. 이를 통해 좌표 압축을 수행한 결과를 compressed_points에 저장
+// 3. s[i][j]: (0,0)부터 (i, j)까지의 좌표 개수
+//    s[i][j] = s[i - 1][j] + s[i][j - 1] - s[i][j]
+//    if (i,j) in compressed_points s[i][j] += 1
+// for i in range(0..len(compressed_points)):
+//   for j in range(i + 1..len(compressed_points)):
+//     x1, y1 = compressed_points[i]
+//     x2, y2 = compressed_points[j]
+//     if s[x1][y1] - s[x2][y2] == 0:
+//       total += 1
+// return total
