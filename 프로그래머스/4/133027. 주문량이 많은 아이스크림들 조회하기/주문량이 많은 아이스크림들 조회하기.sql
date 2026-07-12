dacee95f6c2f649bd07097905c7edf866ca46f63
Chan <qufstar46@gmail.com>
2026-07-12 18:09:30 +0900

    [level 4] Title: 주문량이 많은 아이스크림들 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
+++ 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
@@ -1,6 +1,4 @@
 -- 코드를 입력하세요
-# PRIMARY KEY: FLAVOR
-# FIRST_HALF TABLE의 SHIPMENT_ID는 JULY TABLE의 SHIPMENT_ID의 외래키
 
 # 1. 달별 맛 주문량 총합을 구하기 위해, JULY_TOTAL_ORDER_INFO 테이블 만들기 (group by, sum 사용)
 # 2. 상반기 주문량 총합과 7월 주문량 총합을 더한 TOTAL_ORDER_INFO 테이블 만들기 (LEFT OUTER JOIN 사용하여 값 엇는 경우에도 대응하기)
@@ -34,4 +32,11 @@ LIMIT 3;
 # peach 2450 500 = 2950
 # caramel 2600 460 = 3060
 
+# 존재하는 맛은 없다.
+# 왜냐하면 first_half에 존재하는 shipment_id는 항상 july에 존재하잖아. 근데 shipment_id 가 정해질 경우 대응하는 flavor도 같이 정해지니까
 
+# 따라서 first_half에 존재하는 맛을 모두 커버하기 위해 left outer join을 사용하여야 한다.
+
+# 정정: july table의 flavor는 first_half 테이블의 flavor의 외래키이다.
+#      => july table의 flavor는 모두 first_half 테이블에 들어있다.
+#      => 따라서 first_halfe 테이블의 flavor를 모두 포함시키는 left outer join을 하는 것이 의미상 자연스럽다.
