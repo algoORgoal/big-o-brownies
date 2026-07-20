f64ec459c0cb7c416520de798bc4122bd104c012
Chan <qufstar46@gmail.com>
2026-07-20 21:35:50 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,17 @@
+-- 코드를 입력하세요
+
+# 2022년 10월 5일에 등록된 중고거래 게시물
+
+SELECT
+    BOARD_ID,
+    WRITER_ID,
+    TITLE,
+    PRICE,
+    CASE
+        WHEN STATUS = 'SALE' THEN '판매중'
+        WHEN STATUS = 'RESERVED' THEN '예약중'
+        WHEN STATUS = 'DONE' THEN '거래완료'
+    END AS STATUS
+FROM USED_GOODS_BOARD
+WHERE YEAR(CREATED_DATE) = 2022 && MONTH(CREATED_DATE) = 10 && DAY(CREATED_DATE) = 5
+ORDER BY BOARD_ID DESC;
